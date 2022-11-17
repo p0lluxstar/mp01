bb40724a13ffe99d9c91a1ec81198d58edadc35d
global-p0lluxstar <global-web-registra@yandex.ru>
2022-11-17 23:40:52 +0300

    h-171122-23:40

--- src/js/modules/callback.js
+++ src/js/modules/callback.js
@@ -6,18 +6,19 @@ const callback = () => {
         const modal = document.querySelector(modalSelector);
         const close = document.querySelector(closeSelector);
 
-        trigger.forEach(item =>{
+        trigger.forEach(item =>{ 
             item.addEventListener('click', function(e){
                 if (e.target){ // отменяет перезагрузку страницы при клике на ссылку
                     e.preventDefault();
                 }
                 modal.style.display = 'block';
+                document.querySelector('.data_forms').style.display = 'block';
                 document.body.classList.add('modal-open');
                 /* document.body.style.overflow = 'hidden'; */
             });
         })
 
-        close.addEventListener('click', function(){
+        close.addEventListener('click', function(){ // функция закрывает модальное окно при нажатии на крестик
             modal.style.display = 'none';
             document.body.classList.remove('modal-open');
             /* document.body.style.overflow = ''; */
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -3,25 +3,13 @@ const forms = () =>{
     const form = document.querySelectorAll('form');
     const inputs = document.querySelectorAll('input');
     const checkPhone = document.querySelectorAll('input[name="user_phone"]');
+    const valueInput = document.querySelectorAll('.control_input');
 
 
-    checkPhone.forEach(item => {
+    checkPhone.forEach(item => { // функция проверка ввода номера телефона, если вводится не цифра то ничего не будет прописываться
 
-        /* const valueInput = document.querySelectorAll('.control_input');
-        const btnInput = document.querySelector('.form_button'); */
-
-        console.log(item)
         item.addEventListener('input' , () => {
             item.value = item.value.replace(/\D/, '') // метод replace() выполняет внутри строки поиск с использованием регулярного выражения (объект RegExp), или строкового значения и возвращает новую строку, в которой будут заменены найденные значения
-       
-/*         for(let i = 0; i < valueInput.length; i++){ // цикл который проверяет все поля формы на заполняемость
-            if(valueInput[i].value == ''){
-                valueInput[i].classList.add('error');
-            } else {
-                valueInput[i].classList.remove('error');
-            }
-        }  */
-       
         }) 
     })
 
@@ -31,17 +19,19 @@ const forms = () =>{
         failure: 'Что-то пошло не так...'
     }
 
-    const postData = async (url, data) => {
-        document.querySelector('.status').textContent = message.loading;
-        let res = await fetch(url, {
+    const postData = async (url, data) => { // функция для отравки запроса на сервер, url - это адреус куда будут уходить данные и data это сами данные. async говорит о том что в функции есть асинхронные операции
+        document.querySelector('.status').textContent = message.loading; // Свойство textContent позволяет считывать или задавать текстовое содержимое элемента
+        let res = await fetch(url, { // await заставит интерпретатор JavaScript ждать до тех пор, пока промис справа от await не выполнится
             method: "POST",
             body: data
         });
 
+        // С помощью функции fetch() можно отправлять сетевые запросы на сервер — как получать, так и отправлять данные
+
         return await res.text();
     }
 
-    const clearInputs = () => {
+    const clearInputs = () => { // функция очищает инпуты после отравки
         inputs.forEach(item => {
             item.value = '';
         })
@@ -49,34 +39,51 @@ const forms = () =>{
 
     form.forEach(item => {
 
-        item.addEventListener('submit', (e) => {
+        item.addEventListener('submit', (e) => { //submit означает срабатывание события при отравки формы
             e.preventDefault();
 
-            let statusMessage = document.createElement('div');
-            statusMessage.classList.add('status');
-            item.appendChild(statusMessage);
+            let sumEmptySting = 0;
 
-           
-            const formData = new FormData(item);
-
-            postData('assets/server.php', formData)
-            .then(res => {
-                console.log(res)
-                statusMessage.textContent = message.success;
+            inputs.forEach(items => { // функция считает сколько не заполненных инпутов
+                if ( items.value == ''){
+                   sumEmptySting ++;
+                } 
             })
 
-            .catch(() => statusMessage.textContent = message.failure)
-            .finally(() =>{
-                clearInputs();
-                setTimeout(() => {
-                    statusMessage.remove()
-                }, 5000);
-            })
+            if ( sumEmptySting > 0){
+                for(let i = 0; i < valueInput.length; i++){ // цикл который проверяет все поля формы на заполняемость
+                    if(valueInput[i].value == ''){
+                        valueInput[i].classList.add('error');
+                    } else {
+                        valueInput[i].classList.remove('error');
+                    }
+                }  
+            } else {
+                let statusMessage = document.createElement('div');
+                statusMessage.classList.add('status');
+                item.appendChild(statusMessage);
 
+                const formData = new FormData(item);
+
+                postData('assets/server.php', formData)
+                .then(res => {
+                    console.log(res)
+                    statusMessage.textContent = message.success;
+                    document.querySelector('.data_forms').style.display = 'none';
+                })
+
+                .catch(() => statusMessage.textContent = message.failure) // метод .catch() объекта Promise добавляет обработчик или обработчики, которые будут вызваны при изменении состояния объекта Promise на rejected (выполнение отклонено).
+                .finally(() =>{  // метод .finally() объекта Promise добавляет обработчик, который будет вызван вне зависимости от того с каким результатом объект Promise был выполнен (rejected - выполнение отклонено, или fulfilled - успешное выполнение
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                        document.querySelector('.popup').style.display = 'none';
+                    }, 5000);
+                })
+            }
         })
     })
 
-    
 }
 
 export default forms;
